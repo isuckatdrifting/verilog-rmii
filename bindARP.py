11ef75206eee138c0f00f30ed4df76a5cbc9fc41
geralt <paladin.heleth@gmail.com>
2020-06-11 10:48:33 +0800

    add comments in bindARP

--- bindARP.py
+++ bindARP.py
@@ -3,3 +3,4 @@ import os
 os.system('echo Binding dev board mac to temp arp list...')
 os.system('arp -s 192.168.3.123 00:00:5e:00:fa:ce')
 os.system('echo Binded 192.168.3.123 to 00:00:5e:00:fa:ce')
+os.system('echo Remember to clear the arp cache by running -- sudo arp -d 192.168.3.123')
