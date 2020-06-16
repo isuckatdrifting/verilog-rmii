edc9932a3d9964a798e8ec92193322377257278e
geralt <paladin.heleth@gmail.com>
2020-06-16 12:46:38 +0800

    add long sp packet testcase

--- crc32d4.py
+++ crc32d4.py
@@ -3,7 +3,7 @@ import binascii
 def crc2hex(crc):
     return '%08x' % (binascii.crc32(binascii.a2b_hex(crc)) & 0xffffffff)
 
-data = "00005e00 facef076 1ca94d2e 08004500 004651f7 40004011 6096c0a8 0366c0a8 037bbda7 04d20032 00000000 01000080 64000081 14000082 02000083 01000084 19900185 19900186 00f0ff87 01000088 01000089"
+data = "00005e00 facef076 1ca94d2e 08004500 004651f7 40004011 6096c0a8 0366c0a8 037bbda7 04d20032 00000000 01000080 e8030081 14000082 02000083 01000084 faa00f85 faa00f86 00f0ff87 01000088 01000089"
 
 print("-" * 50)
 print("data to process: " + "\033[31m" + "Note that the Least significant Byte in a word comes first (Big Endian) in CRC calculating" + "\033[0m")
